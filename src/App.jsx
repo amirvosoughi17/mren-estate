af5cb75dc164cba83d29ea1199ce9b17981ec82c
amir <amir11.vosoughi@gmail.com>
2023-10-09 22:10:39 +0330

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Profile from './pages/Profile'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
+    <Header />
       <Routes>
          <Route  path='/' element={<Home />}/>
          <Route  path='/profile' element={<Profile />}/>
