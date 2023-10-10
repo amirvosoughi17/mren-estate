cb3d0393f10b635af03ea36621d4065f066a5764
EhsanMasoumipor <emasoumipor@gmail.com>
2023-10-10 21:29:42 +0330

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
-app.listen(3000 , () => {
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("App connected to MongoDB"))
+    .catch((error) => {
+        console.log(`DB Connection Error error: ${error.message}`)
+    });
+app.listen(3000, () => {
     console.log('server is running on port 3000')
-}
-);   
+});
